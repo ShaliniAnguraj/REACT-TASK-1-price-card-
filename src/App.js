1db10d0d58916b0da3bea0dcb7ce1990f5ef6453
SHALINI ANGURAJ <arshalini298@gmail.com>
2022-12-08 11:27:54 +0530

    card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./Card";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  let data = [
+    {
+      plan: "Free",
+      price: "0",
+
+      features: [
+        {
+          title: "Single User",
+          condition: true,
+        },
+        {
+          title: "5GB Storage",
+          condition: true,
+        },
+        {
+          title: "Unlimited Public Projects",
+          condition: true,
+        },
+        {
+          title: "Community Access",
+          condition: true,
+        },
+        {
+          title: "Unlimited Private Projects",
+          condition: false,
+        },
+        {
+          title: "Dedicated phone support",
+          condition: false,
+        },
+        {
+          title: "Free Subdomain",
+          condition: false,
+        },
+        {
+          title: "Monthly status reports",
+          condition: false,
+        },
+      ],
+    },
+    {
+      plan: "Plus",
+      price: "9",
+      features: [
+        {
+          title: <b>5 Users</b>,
+          condition: true,
+        },
+        {
+          title: "50GB Storage",
+          condition: true,
+        },
+        {
+          title: "Unlimited Public Projects",
+          condition: true,
+        },
+        {
+          title: "Community Access",
+          condition: true,
+        },
+        {
+          title: "Unlimited Private Projects",
+          condition: true,
+        },
+        {
+          title: "Dedicated phone support",
+          condition: true,
+        },
+        {
+          title: " Free Subdomain",
+          condition: true,
+        },
+        {
+          title: "Monthly status reports",
+          condition: false,
+        },
+      ],
+    },
+    {
+      plan: "Pro",
+      price: "49",
+      features: [
+        {
+          title: <b>Unlimited Users</b>,
+          condition: true,
+        },
+        {
+          title: "150GB Storage",
+          condition: true,
+        },
+        {
+          title: "Unlimited Public Projects",
+          condition: true,
+        },
+        {
+          title: "Community Access",
+          condition: true,
+        },
+        {
+          title: "Unlimited Private Projects",
+          condition: true,
+        },
+        {
+          title: "Dedicated phone support",
+          condition: true,
+        },
+        {
+          title: "Unlimited Free Subdomain",
+          condition: true,
+        },
+        {
+          title: "Monthly status reports",
+          condition: true,
+        },
+      ],
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+          {data.map((price) => {
+            return <Card price={price}></Card>;
+          })}
+        </div>
+      </div>
+    </section>
   );
 }
 
